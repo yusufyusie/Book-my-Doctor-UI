182ececc9606a74f194f5bcef073e27185f0e458
Fombi Favour <fombifavour@gmail.com>
2024-02-27 08:00:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Signin from './routes/Signin';
 import Signup from './routes/Signup';
 import Home from './routes/Home';
 import Details from './routes/Details';
+import AddDoctor from './routes/AddDoctor';
 import Layout from './components/Layout';
 
 function App() {
   return (
     <Routes>
-      <Route path="sign_in" element={<Signin />} />
+      <Route path="/" element={<Signin />} />
       <Route path="sign_up" element={<Signup />} />
-      <Route path="/" element={<Layout />}>
+      <Route path="doctors" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="details" element={<Details />} />
+        <Route path="new" element={<AddDoctor />} />
+        <Route path="doctors/:doctorId" element={<Details />} />
       </Route>
       <Route />
     </Routes>
