58dc155ce335739b8ef6419fab34fb8701ff77c6
yusufyusie <yesuf023@gmail.com>
2024-02-27 17:16:26 +0300

    Add new-appointment to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 import {
   FaVimeoV,
   FaTwitter,
@@ -9,29 +11,33 @@ import {
 
 import Logo from '../assets/doc_logo.jpg';
 
-const Navbar = () => (
-  <section className="border-r-2 h-screen p-2">
-    {/* logo */}
-    <img src={Logo} alt="logo" className="w-52" />
-    {/* nav menu */}
-    <div className="flex flex-col mt-10 gap-3">
-      <span className="text-gray-800 text-xl font-bold">MODELS</span>
-      <span className="text-gray-800 text-xl font-bold">LIFESTYLE</span>
-      <span className="text-gray-800 text-xl font-bold">SHOP</span>
-      <span className="text-gray-800 text-xl font-bold">TEST DRIVE</span>
-    </div>
-    {/* socials */}
-    <footer className="absolute bottom-10">
-      <div className="flex justify-center gap-4">
-        <FaTwitter />
-        <FaFacebookF />
-        <FaGooglePlusG />
-        <FaVimeoV />
-        <FaPinterestP />
+function Navbar() {
+  return (
+    <section className="border-r-2 h-screen p-2">
+      {/* logo */}
+      <img src={Logo} alt="logo" className="w-52" />
+      {/* nav menu */}
+      <div className="flex flex-col mt-10 gap-3">
+        <span className="text-gray-800 text-xl font-bold">MODELS</span>
+        <span className="text-gray-800 text-xl font-bold">LIFESTYLE</span>
+        <NavLink to="/new-appointment">
+          <span className="text-gray-800 text-xl font-bold">Book an Appointment</span>
+        </NavLink>
+        <span className="text-gray-800 text-xl font-bold">TEST DRIVE</span>
       </div>
-      <span className="text-xs">copyright 2024 microverse final capstone</span>
-    </footer>
-  </section>
-);
+      {/* socials */}
+      <footer className="absolute bottom-10">
+        <div className="flex justify-center gap-4">
+          <FaTwitter />
+          <FaFacebookF />
+          <FaGooglePlusG />
+          <FaVimeoV />
+          <FaPinterestP />
+        </div>
+        <span className="text-xs">copyright 2024 microverse final capstone</span>
+      </footer>
+    </section>
+  );
+}
 
 export default Navbar;
