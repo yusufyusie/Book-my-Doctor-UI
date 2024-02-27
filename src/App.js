5dd8fafdb0897da25dcd654db817e8a2e24d76d2
yusufyusie <yesuf023@gmail.com>
2024-02-27 17:13:41 +0300

    Add NewAppointmentPage to the app component

--- src/App.js
+++ src/App.js
@@ -5,20 +5,19 @@ import Signup from './routes/Signup';
 import Home from './routes/Home';
 import Details from './routes/Details';
 import Layout from './components/Layout';
+import NewAppointmentPage from './routes/NewAppointmentPage'; // make sure the path is correct
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="sign_in" element={<Signin />} />
-        <Route path="sign_up" element={<Signup />} />
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="details" element={<Details />} />
-        </Route>
-        <Route />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="sign_in" element={<Signin />} />
+      <Route path="sign_up" element={<Signup />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="details" element={<Details />} />
+        <Route path="new-appointment" element={<NewAppointmentPage />} />
+      </Route>
+    </Routes>
   );
 }
 
