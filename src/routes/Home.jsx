fd5290e36205dbc24b3527ce89ca4b69186085e1
Fombi Favour <fombifavour@gmail.com>
2024-02-28 12:46:42 +0100

    Update Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => (
   <div>
-    <h2 className="text-3xl uppercase">latest models</h2>
+    <h2 className="text-3xl uppercase">doctors</h2>
   </div>
 );
 
