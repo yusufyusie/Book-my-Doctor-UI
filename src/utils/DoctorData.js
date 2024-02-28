ccde23204dd2fca3b1e83d971ed776bfe61bc3dc
Fombi Favour <fombifavour@gmail.com>
2024-02-28 12:46:46 +0100

    Create DoctorData.js

---
+++ src/utils/DoctorData.js
@@ -0,0 +1,34 @@
+const doctor = [
+  {
+    id: 1,
+    name: 'Amanda',
+    image: 'https://images.pexels.com/photos/5452201/pexels-photo-5452201.jpeg?auto=compress&cs=tinysrgb&w=600',
+    speciality: 'Cardiology',
+  },
+  {
+    id: 2,
+    name: 'Charles',
+    image: 'https://images.pexels.com/photos/8460157/pexels-photo-8460157.jpeg?auto=compress&cs=tinysrgb&w=600',
+    speciality: 'Cardiology',
+  },
+  {
+    id: 3,
+    name: 'Peter',
+    image: 'https://images.pexels.com/photos/8460157/pexels-photo-8460157.jpeg?auto=compress&cs=tinysrgb&w=600',
+    speciality: 'Neurology',
+  },
+  {
+    id: 4,
+    name: 'Christopher',
+    image: 'https://images.pexels.com/photos/8460157/pexels-photo-8460157.jpeg?auto=compress&cs=tinysrgb&w=600',
+    speciality: 'Dentist',
+  },
+  {
+    id: 1,
+    name: 'Sandra',
+    image: 'https://images.pexels.com/photos/4167541/pexels-photo-4167541.jpeg?auto=compress&cs=tinysrgb&w=600',
+    speciality: 'Dentist',
+  },
+];
+
+export default doctor;
