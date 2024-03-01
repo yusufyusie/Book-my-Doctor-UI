2f102e770faff0b35e854b4ed69d5d0e40169230
hydaline-code <djougancharlen@gmail.com>
2024-03-01 02:56:35 +0100

    update routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Signin from './routes/Signin';
-import Signup from './routes/Signup';
-import Home from './routes/Home';
-import Details from './routes/Details';
-import Layout from './components/Layout';
+  import './App.css';
+  import { Route, Routes } from 'react-router-dom';
+  import Signup from './routes/Signup';
+  import Home from './routes/Home';
+  import Details from './routes/Details';
+  import Layout from './components/Layout';
+  import Login from './routes/Login';
+  import Signin from './routes/Signin';
 
-function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="sign_in" element={<Signin />} />
-        <Route path="sign_up" element={<Signup />} />
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="details" element={<Details />} />
-        </Route>
-        <Route />
-      </Routes>
-    </>
-  );
-}
+  function App() {  
+    return (
+      <>
+        <Routes>
+        <Route path="/signin" component={Signin} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/" element={<Login />} />
+          <Route path="/doctors" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="details" element={<Details />} />
+          </Route>
+          <Route />
+        </Routes>
+      </>
+    );
+  }
 
-export default App;
+  export default App;
