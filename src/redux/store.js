6b10d0d3142f49689703d708bc3fc6255ac66d7b
Fombi Favour <fombifavour@gmail.com>
2024-03-02 15:01:36 +0100

    fix errors

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { logger } from 'redux-logger';
 import doctorReducer from './doctorReducer';
+import userReducer from './user/userSlice';
 
 const store = configureStore({
   reducer: {
     doctorData: doctorReducer,
+    user: userReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
