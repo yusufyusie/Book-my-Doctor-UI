5488ee7f2e3ce5d2044f081460bd574ca77026d9
Fombi Favour <fombifavour@gmail.com>
2024-03-04 11:07:37 +0100

    Update userSlice.js

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -1,29 +1,48 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const signinUrl = 'http://127.0.0.1:3001/login';
 const signupUrl = 'http://127.0.0.1:3001/signup';
 
 const initialState = {
-  users: null,
+  userContent: [],
   status: 'idle',
   error: null,
 };
 
 export const signupUser = createAsyncThunk('user/signup', async (data) => {
+  const addUser = {
+    user: {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      password_confirmation: data.password_confirmation,
+    },
+  };
+
   try {
-    const response = await axios.post(signupUrl, data);
-    alert('User Created Successfully');
-    return response.data;
+    const response = await axios.post(signupUrl, addUser);
+    toast.success('Signup successful. Please sign in.');
+    return { data, response };
   } catch (error) {
     throw error.response.data;
   }
 });
 export const signinUser = createAsyncThunk('user/signin', async (data) => {
-  try {
-    const response = await axios.post(signinUrl, data);
+  const loginUser = {
+    user: {
+      email: data.email,
+      password: data.password,
+    },
+  };
 
-    return response.data;
+  try {
+    const response = await axios.post(signinUrl, loginUser);
+    if (response.data.token) {
+      localStorage.setItem('userdata', response.data.token, response.data.user);
+    }
+    return { data, response };
   } catch (error) {
     if (error.response && error.response.status === 401) {
       throw new Error('Invalid email or password. Please try again.');
@@ -36,17 +55,27 @@ export const signinUser = createAsyncThunk('user/signin', async (data) => {
 const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {},
+  reducers: {
+    logout: (state) => {
+      state.user = [];
+    },
+    setName: (state, action) => {
+      const userId = action.payload;
+      const user = state.userContent.find((item) => item.id === userId);
+      state.userName = user ? user.name : null;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(signupUser.fulfilled, (state, action) => {
       state.status = 'succeeded';
       state.user = action.payload;
     }).addCase(signinUser.fulfilled, (state, action) => {
       state.status = 'succeeded';
-      state.user = action.payload.status.data.id;
-      localStorage.setItem('user_id', JSON.stringify(action.payload.status.data.id));
+      state.user = action.payload.response.data.user.id;
+      localStorage.setItem('userdata', JSON.stringify(action.payload.response.data.user.id));
     });
   },
 });
 
 export default userSlice.reducer;
+export const { logout, setName } = userSlice.actions;
