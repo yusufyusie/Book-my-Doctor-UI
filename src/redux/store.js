a2588b101cdd2cf41b2e2acdf0b774942f6fa952
yusufyusie <yesuf023@gmail.com>
2024-03-04 17:22:46 +0300

    Fix the conflicts are resolved and all files are added, commit the merge:

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-<<<<<<< HEAD
-import { logger } from 'redux-logger';
-import doctorReducer from './doctorReducer';
-import reservationsReducer from './reservations/reservationsSlice';
-import authReducer from './auth/authSlice';
-
-const store = configureStore({
-  reducer: {
-    doctorData: doctorReducer,
-    reservations: reservationsReducer,
-    auth: authReducer,
-=======
 import doctorReducer from './doctor/doctorSlice';
 import userReducer from './user/userSlice';
 
@@ -18,7 +6,6 @@ const store = configureStore({
   reducer: {
     user: userReducer,
     doctor: doctorReducer,
->>>>>>> origin/redux-feature
   },
 });
 
