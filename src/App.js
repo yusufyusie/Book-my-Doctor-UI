48ecda8a6283596cdd815ac696ebc2c56df4cbb9
Fombi Favour <fombifavour@gmail.com>
2024-03-04 11:06:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Signin from './routes/Signin';
 import Signup from './routes/Signup';
 import Home from './routes/Home';
@@ -11,24 +12,28 @@ import Layout from './components/Layout';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Signin />} />
-      <Route path="sign_up" element={<Signup />} />
-      <Route path="doctors" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="new" element={<AddDoctor />} />
-        <Route path=":doctorId" element={<Details />} />
-        <Route path="delete" element={<DeleteDoc />} />
-      </Route>
-      <Route path="reservations" element={<Layout />}>
-        <Route index element={<Reservation />} />
-        <Route path="new" element={<NewReservation />} />
-      </Route>
-      <Route />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Signin />} />
+        <Route path="sign_up" element={<Signup />} />
+        <Route path="doctors" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="new" element={<AddDoctor />} />
+          <Route path=":doctorId" element={<Details />} />
+          <Route path="delete" element={<DeleteDoc />} />
+        </Route>
+        <Route path="reservations" element={<Layout />}>
+          <Route index element={<Reservation />} />
+          <Route path="new" element={<NewReservation />} />
+        </Route>
+        <Route />
+      </Routes>
+      <ToastContainer />
+    </>
   );
 }
 
