70f51e622b54143b690209a6df94b42c1bb7e40e
Fombi Favour <fombifavour@gmail.com>
2024-03-04 11:07:28 +0100

    fix errors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import {
   FaVimeoV,
   FaTwitter,
@@ -9,44 +9,56 @@ import {
   // FaTimes,
 } from 'react-icons/fa';
 import { TiSocialGooglePlus } from 'react-icons/ti';
+import { useDispatch } from 'react-redux';
 import navMenu from '../utils/NavbarData';
+import { logout } from '../redux/user/userSlice';
 
 import Logo from '../assets/doc_logo.jpg';
 
-const Navbar = () => (
-  <section className="border-r-2 h-screen">
-    {/* logo */}
-    <div className="p-2">
-      <img src={Logo} alt="logo" className="w-52 rounded-md" />
-    </div>
-    {/* nav menu */}
-    <div className="flex flex-col mt-10 ml-4 gap-3">
-      {navMenu.map((menu) => (
-        <NavLink
-          key={menu.id}
-          to={menu.path}
-          className={({ isActive }) => `text-gray-800 p-1 text-xl uppercase font-bold no-underline${isActive ? 'text-white bg-lime-500' : ''}`}
-        >
-          {menu.name}
-        </NavLink>
-      ))}
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-      <NavLink to="/">
-        <span className="uppercase text-gray-800 font-bold text-xl mt-2">logout</span>
-      </NavLink>
-    </div>
-    {/* socials */}
-    <footer className="absolute bottom-10 left-4">
-      <div className="flex justify-center gap-4">
-        <FaTwitter />
-        <FaFacebookF />
-        <TiSocialGooglePlus />
-        <FaVimeoV />
-        <FaPinterestP />
+  const logoutUser = () => {
+    dispatch(logout);
+    localStorage.removeItem('userdata');
+    navigate('/');
+  };
+
+  return (
+    <section className="border-r-2 h-screen">
+      {/* logo */}
+      <div className="p-2">
+        <img src={Logo} alt="logo" className="w-52 rounded-md" />
       </div>
-      <span className="text-xs">&copy; 2024 microverse final capstone</span>
-    </footer>
-  </section>
-);
+      {/* nav menu */}
+      <div className="flex flex-col mt-10 ml-4 gap-3">
+        {navMenu.map((menu) => (
+          <NavLink
+            key={menu.id}
+            to={menu.path}
+            className={({ isActive }) => `text-gray-800 p-1 text-xl uppercase font-bold no-underline${isActive ? 'text-white bg-lime-500' : ''}`}
+          >
+            {menu.name}
+          </NavLink>
+        ))}
+        <button type="button" onClick={logoutUser}>
+          <span className="uppercase text-gray-800 font-bold text-xl mt-2">logout</span>
+        </button>
+      </div>
+      {/* socials */}
+      <footer className="absolute bottom-10 left-4">
+        <div className="flex justify-center gap-4">
+          <FaTwitter />
+          <FaFacebookF />
+          <TiSocialGooglePlus />
+          <FaVimeoV />
+          <FaPinterestP />
+        </div>
+        <span className="text-xs">&copy; 2024 microverse final capstone</span>
+      </footer>
+    </section>
+  );
+};
 
 export default Navbar;
