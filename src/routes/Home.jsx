599dd7425d8c12d0d49cca64cb1caa0d8d708792
Fombi Favour <fombifavour@gmail.com>
2024-03-05 21:04:54 +0100

    Update Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,6 +6,7 @@ import { TiChevronLeftOutline, TiChevronRightOutline } from 'react-icons/ti';
 import { fetchDoctors } from '../redux/doctor/doctorSlice';
 import useWindowSize from '../hooks/windowsSize';
 import Doctor from '../components/Doctor';
+import Greetings from '../components/Greetings';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -40,28 +41,31 @@ const Home = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center mt-5">
-      <h2 className="text-3xl font-bold uppercase">doctors</h2>
-      <h4 className="text-xl text-gray-400">Select a doctor</h4>
+    <>
+      <Greetings />
+      <div className="flex flex-col justify-center items-center mt-5">
+        <h2 className="text-3xl font-bold uppercase">doctors</h2>
+        <h4 className="text-xl text-gray-400">Select a doctor</h4>
 
-      <div className="mt-10">
-        <div className="flex items-center justify-center">
-          { /* eslint-disable-next-line react/jsx-props-no-spreading */ }
-          <Slider {...settings} className="w-[65rem]">
-            {doctors?.map((item) => (
-              <Link to={`/doctors/${item.id}`} key={item.id}>
-                <Doctor
-                  name={item.name}
-                  image={item.image_url}
-                  spec={item.specialization}
-                  fee={item.cost}
-                />
-              </Link>
-            ))}
-          </Slider>
+        <div className="mt-10">
+          <div className="flex items-center justify-center">
+            { /* eslint-disable-next-line react/jsx-props-no-spreading */ }
+            <Slider {...settings} className="w-[65rem]">
+              {doctors?.map((item) => (
+                <Link to={`/doctors/${item.id}`} key={item.id}>
+                  <Doctor
+                    name={item.name}
+                    image={item.image_url}
+                    spec={item.specialization}
+                    fee={item.cost}
+                  />
+                </Link>
+              ))}
+            </Slider>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
