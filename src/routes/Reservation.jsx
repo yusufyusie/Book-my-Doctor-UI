e438342978bc941516f5e4eb3a1090de7c5fdff4
yusufyusie <yesuf023@gmail.com>
2024-03-05 10:11:08 +0300

    Add reponsivessness

--- src/routes/Reservation.jsx
+++ src/routes/Reservation.jsx
@@ -14,34 +14,60 @@ const Reservation = () => {
     slidesToScroll: 1,
     nextArrow: <TiChevronRightOutline color="#000" />,
     prevArrow: <TiChevronLeftOutline color="#000" />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
 
   const appointImg = 'https://images.pexels.com/photos/7089401/pexels-photo-7089401.jpeg?auto=compress&cs=tinysrgb&w=600';
 
   return (
-    <div className="flex flex-col justify-center items-center mt-5">
+    <div className="flex flex-col justify-center items-center mt-5 px-4 sm:px-6 lg:px-8">
       <h2 className="text-3xl font-bold uppercase">my reservations</h2>
 
       <ul className="mt-10">
         <div className="flex items-center justify-between gap-10">
           {/* card slide */}
           { /* eslint-disable-next-line react/jsx-props-no-spreading */ }
-          <Slider {...settings} className="w-[65rem]">
+          <Slider {...settings} className="w-full sm:w-[65rem]">
             {reservation.map((item) => {
               const doctor = doctors.find((doc) => doc.id === item.doctor_id);
               return (
                 <li key={item.id}>
                   <div>
-                    <img src={appointImg} alt="appointment" className="w-80 h-48 rounded-xl" />
+                    <img src={appointImg} alt="appointment" className="w-full h-32 sm:h-48 rounded-xl" />
                   </div>
-                  <div className="flex flex-col mt-4">
+                  <div className="flex flex-col mt-4 text-sm sm:text-base">
                     {/* name of doctor appointed */}
                     <div>
                       <span className="flex items-center gap-3 text-lg text-slate-500">
                         <FaUserDoctor />
                         Doctor Appointed
                       </span>
-                      <p className="text-xl font-semibold ml-7 text-gray-700">
+                      <p className="font-semibold ml-7 text-gray-700">
                         Dr.
                         {' '}
                         {doctor.name}
@@ -53,7 +79,7 @@ const Reservation = () => {
                         <FaCalendarDays />
                         Date
                       </span>
-                      <p className="text-xl font-semibold ml-7 text-gray-700">{item.date}</p>
+                      <p className="font-semibold ml-7 text-gray-700">{item.date}</p>
                     </div>
                     {/* time of appointment */}
                     <div>
@@ -61,15 +87,15 @@ const Reservation = () => {
                         <FaClock />
                         Time
                       </span>
-                      <p className="text-xl font-semibold ml-7 text-gray-700">{item.time}</p>
+                      <p className="font-semibold ml-7 text-gray-700">{item.time}</p>
                     </div>
                     {/* reason for the appointment */}
                     <div>
                       <span className="flex items-center gap-3 text-lg text-slate-500">
                         <FaFirstAid />
-                        Doctor Appointed
+                        Reason
                       </span>
-                      <p className="text-xl font-semibold ml-7 text-gray-700">{item.reason}</p>
+                      <p className="font-semibold ml-7 text-gray-700">{item.reason}</p>
                     </div>
                   </div>
                 </li>
