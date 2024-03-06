a00469326d49abbd2bc3a07c4a806d4fbcd45864
yusufyusie <yesuf023@gmail.com>
2024-03-06 13:56:28 +0300

    Update reservation of the doctors

--- src/routes/Reservation.jsx
+++ src/routes/Reservation.jsx
@@ -1,31 +1,10 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import Slider from 'react-slick';
-import { FaUserDoctor, FaCalendarDays } from 'react-icons/fa6';
-import { FaDollarSign } from 'react-icons/fa';
-import { TiChevronLeftOutline, TiChevronRightOutline } from 'react-icons/ti';
 import { fetchAppoint } from '../redux/appointment/appointmentSlice';
-import useWindowSize from '../hooks/windowsSize';
 
 const Reservation = () => {
   const dispatch = useDispatch();
   const appoint = useSelector((state) => state.appointment.appointContent.data || []);
-  const { windowWidth } = useWindowSize();
-
-  let slidesToshow = 3;
-  if (windowWidth > 1440) {
-    slidesToshow = 1;
-  }
-
-  const settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: slidesToshow,
-    slidesToScroll: 1,
-    nextArrow: <TiChevronRightOutline color="#000" />,
-    prevArrow: <TiChevronLeftOutline color="#000" />,
-  };
 
   useEffect(() => {
     dispatch(fetchAppoint());
@@ -42,58 +21,39 @@ const Reservation = () => {
   const appointImg = 'https://images.pexels.com/photos/7089401/pexels-photo-7089401.jpeg?auto=compress&cs=tinysrgb&w=600';
 
   return (
-    <div className="flex flex-col justify-center items-center mt-5">
-      <h2 className="text-3xl font-bold uppercase">my appointments</h2>
-
-      <ul className="mt-10">
-        <div className="flex items-center">
-          { /* eslint-disable-next-line react/jsx-props-no-spreading */}
-          <Slider {...settings} className="w-[65rem]">
-            {appoint.map((item, index) => {
-              const rawdate = item.user.date_of_appointment;
-              const dateObj = new Date(rawdate);
-              const formatDate = dateObj.toISOString().split('T')[0];
-              return (
-                // eslint-disable-next-line react/no-array-index-key
-                <li key={index}>
-                  <div>
-                    <img src={appointImg} alt="appoint" className="w-80 h-48 rounded-xl" />
-                  </div>
-                  <div className="flex flex-col mt-4">
-                    {/* name of doctor appointed */}
-                    <div>
-                      <span className="flex items-center gap-3 text-lg text-slate-500">
-                        <FaUserDoctor />
-                        Doctor Appointed
-                      </span>
-                      <p className="text-xl font-semibold ml-7 text-gray-700">
-                        Dr.
-                        {' '}
-                        {item.doctor.name}
-                      </p>
-                    </div>
-                    {/* date of appointment */}
-                    <div>
-                      <span className="flex items-center gap-3 text-lg text-slate-500">
-                        <FaCalendarDays />
-                        Date
-                      </span>
-                      <p className="text-xl font-semibold ml-7 text-gray-700">{formatDate}</p>
-                    </div>
-                    {/* fee of appointment */}
-                    <div>
-                      <span className="flex items-center gap-3 text-lg text-slate-500">
-                        <FaDollarSign />
-                        Fee of appointment
-                      </span>
-                      <p className="text-xl font-semibold ml-7 text-gray-700">{`$${item.doctor.cost}`}</p>
-                    </div>
-                  </div>
-                </li>
-              );
-            })}
-          </Slider>
-        </div>
+    <div className="flex flex-col gap-12 p-8 mx-auto">
+      <h1 className="text-2xl font-bold">Your appointments</h1>
+      <ul className="flex flex-col gap-8 max-w-xs md:max-w-3xl">
+        {appoint.map((item, index) => {
+          const rawdate = item.user.date_of_appointment;
+          const dateObj = new Date(rawdate);
+          const formatDate = dateObj.toISOString().split('T')[0];
+          return (
+            <li key={index} className="flex flex-col md:flex-row gap-8 bg-green-50 rounded-lg shadow-lg p-8">
+              <div>
+                <img src={appointImg} alt="appoint" className="w-80 h-48 rounded-xl" />
+              </div>
+              <div className="flex flex-col gap-4 mt-4">
+                <h2 className="text-xl font-bold">
+                  Appointment #
+                  {index + 1}
+                </h2>
+                <p className="">
+                  Doctor Appointed:&nbsp;
+                  {item.doctor.name}
+                </p>
+                <p className="">
+                  Date:&nbsp;
+                  {formatDate}
+                </p>
+                <p className="">
+                  Fee of appointment:&nbsp; $
+                  {item.doctor.cost}
+                </p>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
