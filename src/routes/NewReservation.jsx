6c13e6533b3d66cc36b1f42402b9c15cd2240ece
Fombi Favour <fombifavour@gmail.com>
2024-03-06 08:01:45 +0100

    Update NewReservation.jsx

--- src/routes/NewReservation.jsx
+++ src/routes/NewReservation.jsx
@@ -4,9 +4,9 @@ import doctors from '../utils/DoctorData';
 
 function NewReservation() {
   return (
-    <div className="min-h-screen flex flex-col gap-8 justify-center items-center w-full bg-gray-100 py-6 sm:px-6 lg:px-8">
-      <div className="mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+    <div className="min-h-screen flex flex-col gap-8 justify-center items-center md:-mt-28 lg:mt-0 w-screen lg:w-full bg-gray-100 py-6 px-6 lg:px-8">
+      <div className="mx-auto w-11/12 md:w-1/2">
+        <div className="bg-white py-8 px-4 shadow rounded-xl sm:px-10">
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Book an Appointment</h2>
           <form className="mt-8 space-y-6">
             <label htmlFor="doctor" className="block text-sm font-medium text-gray-700">
