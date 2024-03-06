8df7d813744b31be14a35ae1cb771b055a9db468
yusufyusie <yesuf023@gmail.com>
2024-03-06 15:40:19 +0300

    Resolved merge conflicts of adddoctor

--- src/routes/AddDoctor.jsx
+++ src/routes/AddDoctor.jsx
@@ -4,65 +4,6 @@ import { useNavigate } from 'react-router-dom';
 import { v4 as uuid } from 'uuid';
 import { postDoctor } from '../redux/doctor/doctorSlice';
 
-<<<<<<< HEAD
-const AddDoctor = () => (
-  <div className="min-h-screen flex flex-col gap-8 justify-center items-center w-screen lg:w-full bg-gray-100 py-6 px-6 lg:px-8">
-    <div className="mx-auto w-11/12 md:w-1/2">
-      <div className="bg-white -mt-20 md:-mt-72 lg:mt-0 py-8 px-4 shadow rounded-xl sm:px-10">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Add a doctor
-        </h2>
-        <form className="mt-8 space-y-6">
-          <label
-            htmlFor="name"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Name of doctor
-            <div className="mt-1">
-              <input
-                id="name"
-                name="name"
-                type="text"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                required
-              />
-            </div>
-          </label>
-          <label
-            htmlFor="image"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Image URL (e.g. https://www.random/..../)
-            <div className="mt-1">
-              <input
-                id="image"
-                name="image"
-                type="text"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                required
-              />
-            </div>
-          </label>
-          <label
-            htmlFor="speciality"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Doctor speciality
-            <div className="mt-1">
-              <input
-                id="speciality"
-                name="speciality"
-                type="text"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                required
-              />
-            </div>
-          </label>
-          <div>
-            <button
-              type="submit"
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-500 hover:bg-white hover:text-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
-=======
 const AddDoctor = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -90,9 +31,9 @@ const AddDoctor = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col gap-8 justify-center items-center w-full bg-gray-100 py-6 sm:px-6 lg:px-8">
-      <div className="mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+    <div className="min-h-screen flex flex-col gap-8 justify-center items-center w-screen lg:w-full bg-gray-100 py-6 px-6 lg:px-8">
+      <div className="mx-auto w-11/12 md:w-1/2">
+        <div className="bg-white -mt-20 md:-mt-72 lg:mt-0 py-8 px-4 shadow rounded-xl sm:px-10">
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Add a doctor
           </h2>
@@ -100,7 +41,6 @@ const AddDoctor = () => {
             <label
               htmlFor="name"
               className="block text-sm font-medium text-gray-700"
->>>>>>> dev
             >
               Name of doctor
               <div className="mt-1">
