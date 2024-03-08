40f7870e612c573354f192e480df1a87db5949db
Yesuf Fenta <yesuf023@gmail.com>
2024-03-08 17:01:44 +0300

    Update appointmentSlice.js

--- src/redux/appointment/appointmentSlice.js
+++ src/redux/appointment/appointmentSlice.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import getHeaders from '../../api/api_helper';
 
-const appointURL = 'https://book-my-doctor-2jsx.onrender.com/api/appointments';
+const appointURL =  `${process.env.REACT_APP_BASE_URL}/api/appointments';
 
 const initialState = {
   appointContent: '',
